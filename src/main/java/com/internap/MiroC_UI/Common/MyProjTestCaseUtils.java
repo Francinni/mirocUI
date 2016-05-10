68bd5bb8441ace669d556d3efe965d3f332d2893
Francinni <fportuguez@testingsoft.com>
2016-05-10 10:59:00 -0600

    Fix

---
+++ src/main/java/com/internap/MiroC_UI/Common/MyProjTestCaseUtils.java
@@ -0,0 +1,19 @@
+package com.internap.MiroC_UI.Common;
+
+import com.ts.commons.TestCaseUtil;
+import org.testng.annotations.AfterClass;
+
+public class MyProjTestCaseUtils extends TestCaseUtil {
+
+	protected InternapUI uiInstance;
+
+	public MyProjTestCaseUtils(String browserType) {
+		uiInstance = new InternapUI(browserType);
+	}
+
+	@AfterClass
+	public void after() {
+		uiInstance.getDriver().quit();
+	}
+
+}
