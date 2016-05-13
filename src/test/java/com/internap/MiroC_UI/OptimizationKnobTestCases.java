df3e346b42d399b35c1684a93d922df1b02db719
Francinni <fportuguez@testingsoft.com>
2016-05-13 08:36:49 -0600

    class optimization knobs

---
+++ src/test/java/com/internap/MiroC_UI/OptimizationKnobTestCases.java
@@ -0,0 +1,102 @@
+package com.internap.MiroC_UI;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.internap.MiroC_UI.Common.Common;
+import com.internap.MiroC_UI.Common.InternapUI;
+import com.internap.MiroC_UI.Home.ConfigurationPage;
+import com.internap.MiroC_UI.Home.Home;
+import com.internap.MiroC_UI.Home.LoginPage;
+import com.internap.MiroC_UI.Pages.OptimizationKnobPage;
+import com.internap.MiroC_UI.Common.MyProjTestCaseUtils;
+
+
+public class OptimizationKnobTestCases extends MyProjTestCaseUtils {	
+	
+	LoginPage loginPage = InternapUI.goToPortalPage_withFirefox();
+	OptimizationKnobPage optimizatioKnobPage = PageFactory.initElements(
+			uiInstance.getDriver(), OptimizationKnobPage.class);
+	ConfigurationPage configurationPage;
+	Home home;
+
+	public OptimizationKnobTestCases(String browserType) {
+		super(BrowserType.FIREFOX);
+	}
+	
+	@BeforeClass(alwaysRun = true)
+	public void setup() throws Exception {
+		loginPage.login(Common.adminUserName, Common.passWord);
+		home = PageFactory.initElements(uiInstance.getDriver(), Home.class);
+	}
+	
+	
+	/**
+	 * This test case is the equivalent to the Testlink id: MIRO'
+	*/
+	@Test(groups = { "Positive",  "updateKnobs" },priority = 0)
+	public void updateKnobs() {
+
+		using(optimizatioKnobPage = home
+				.goConfigurationTab(uiInstance.getDriver())
+				.goOptimizationKnob(uiInstance.getDriver())
+				.updateKnobs(uiInstance.getDriver(),70, 70, 35, 7))
+				.check(optimizatioKnobPage.assignmentListMustBePresent(uiInstance.getDriver()));
+	} 
+	
+	
+	/**
+	 * This test case is the equivalent to the Testlink id: MIRO'
+	*/
+	@Test(groups = { "Positive", "updateSimulationKnobs" },  dependsOnMethods = "negativeKnobs")
+	public void updateSimulationKnobs() {
+
+		using(optimizatioKnobPage = home
+				.goConfigurationTab(uiInstance.getDriver())
+				.goOptimizationKnob(uiInstance.getDriver())
+				.updateSimulationKnobs(uiInstance.getDriver(),50, 50, 50, 50))
+				.check(optimizatioKnobPage.assignmentListMustBePresent(uiInstance.getDriver()));
+	}
+	
+	
+	//negative test cases
+	/**
+	 * This test case is the equivalent to the Testlink id: MIRO'
+	*/
+	@Test(groups = { "Negative",  "updateKnobs" },dependsOnMethods = "updateKnobs")
+	public void negativeKnobs() {
+		using(optimizatioKnobPage = home
+				.goConfigurationTab(uiInstance.getDriver())
+				.goOptimizationKnob(uiInstance.getDriver())
+				.updateKnobs(uiInstance.getDriver(),-5, -5, -5, -5))
+				.check(optimizatioKnobPage.negativeValuesValidator(uiInstance.getDriver()));
+	}
+	
+	/**
+	 * This test case is the equivalent to the Testlink id: MIRO'
+	*/
+	@Test(groups = { "Negative",  "updateKnobs" },dependsOnMethods = "negativeKnobs")
+	public void overKnobs() {
+		using(optimizatioKnobPage = home
+				.goConfigurationTab(uiInstance.getDriver())
+				.goOptimizationKnob(uiInstance.getDriver())
+				.updateKnobs(uiInstance.getDriver(),101, 101, 101, 101))
+				.check(optimizatioKnobPage.overValuesValidator(uiInstance.getDriver()));
+	}
+	
+	/**
+	 * This test case is the equivalent to the Testlink id: MIRO'
+	*/
+	@Test(groups = { "Positive", "updateSimulationKnobs" },  dependsOnMethods = "updateSimulationKnobs")
+	public void negativeSimulationKnobs() {
+
+		using(optimizatioKnobPage = home
+				.goConfigurationTab(uiInstance.getDriver())
+				.goOptimizationKnob(uiInstance.getDriver())
+				.updateSimulationKnobs(uiInstance.getDriver(),-6, -6, -6, -6))
+				.check(optimizatioKnobPage.assignmentListMustBePresent(uiInstance.getDriver()));
+	}
+
+}//end
